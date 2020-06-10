1c284cfaa6aa829590977afb8bbbdb5b5aa00a0d
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-06-10 18:09:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,8 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
+  monopoly = {railroads: {}}
+
 
 end
 
@@ -9,6 +11,19 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  monopoly = {
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {
+      
+      },
+      names: {
+        
+      }
+    }
+  }
+  
 
 end
 
@@ -17,6 +32,27 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly = {
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
+        
+      },
+      names: {
+        reading_railroad: 
+        
+      }
+    }
+  }
+  
+  
+  
+  
+  
 
 end
 
@@ -25,5 +61,11 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
+  
+  
+  
+  
+  
 
 end
